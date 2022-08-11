4a1d70ad59cc75e7b2d5c58fc87bdab57d6ec300
StephenKennyGains <stephenkennygains@gmail.com>
2022-08-11 11:45:26 +0000

    Set up View, URLS, Model and html for Services

--- services/views.py
+++ services/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Services
 
 # Create your views here.
+
+def all_services(request):
+    """ All Services View """
+
+    services = Services.objects.all()
+
+    context = {
+        'services': services,
+    }
+
+    return render(request, 'services/services.html', context)
