444078e6fef27c1158a6a73ae334a80ed49b644f
StephenKennyGains <stephenkennygains@gmail.com>
2022-08-13 13:44:37 +0000

    Fix up contexts.py and cart Views.py and Add checkout cart html table

--- cart/contexts.py
+++ cart/contexts.py
@@ -1,13 +1,43 @@
-def cart_contents(request):
+from decimal import Decimal
+from django.conf import settings
+from django.shortcuts import get_object_or_404
+from services.models import Service
 
-    cart_items = {}
+
+def cart_contents(request):
+    """ Function for adding Total of items to shopping cart """
+
+    cart_items = []
     total = 0
-    product_count = 0
+    service_count = 0
+    cart = request.session.get('cart', {})
+
+    for item_id, quantity in cart.items():
+        service = get_object_or_404(Service, pk=item_id)
+        total += quantity * service.price
+        service_count += quantity
+        cart_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'service': service,
+        })
+
+    if total < settings.FREE_DELIVERY_THRESHOLD:
+        free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
+    else:
+        delivery = 0
+        free_delivery_delta = 0
+
+    grand_total = delivery + total
 
     context = {
         'cart_items': cart_items,
         'total': total,
-        'product_count': product_count
+        'service_count': service_count,
+        'delivery': delivery,
+        'free_delivery_delta': free_delivery_delta,
+        'free_delivery_threshold': settings.FREE_DELIVERY_THRESHOLD,
+        'grand_total': grand_total,
     }
 
     return context
