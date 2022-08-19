0c3a435b92bcde3829b485232c4d39100d707ac5
StephenKennyGains <stephenkennygains@gmail.com>
2022-08-19 18:02:42 +0000

    Add function to add products as a superadmin on page

--- services/views.py
+++ services/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Service
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from .models import Service, Category
+from .forms import ServiceForm
 
 # Create your views here.
 
@@ -26,3 +28,24 @@ def service_detail(request, service_id):
     }
 
     return render(request, 'services/service_detail.html', context)
+
+
+def add_service(request):
+    """ Add a product to the store """
+    if request.method == 'POST':
+        form = ServiceForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Successfully added service!')
+            return redirect(reverse('add_service'))
+        else:
+            messages.error(request, 'Failed to add Service. Please ensure the form is valid.')
+    else:
+        form = ServiceForm()
+
+    template = 'services/add_service.html'
+    context = {
+        'form': form,
+    }
+
+    return render(request, template, context)
