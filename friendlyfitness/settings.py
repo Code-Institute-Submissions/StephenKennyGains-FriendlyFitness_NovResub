7b80fe38fa7b7fec5f1d47d6bf8a837615d50b57
StephenKennyGains <stephenkennygains@gmail.com>
2022-08-21 17:18:03 +0000

    Prepare for deployment

--- friendlyfitness/settings.py
+++ friendlyfitness/settings.py
@@ -121,9 +121,21 @@ WSGI_APPLICATION = 'friendlyfitness.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+#if "DATABASE_URL" in os.environ:
+#    DATABASES = {
+#        "default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
+#    }
+#else:
+#    DATABASES = {
+#        'default': {
+#            "ENGINE": "django.db.backends.sqlite3",
+#            "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+#        }
+#   }
 
 
 # Password validation
