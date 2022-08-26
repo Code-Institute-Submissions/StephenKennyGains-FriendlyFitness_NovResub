875e97cb75fb2fc6c8c8d707f621028227a516c5
StephenKennyGains <stephenkennygains@gmail.com>
2022-08-26 21:41:12 +0000

    Update from toasts to messages and update layout

--- friendlyfitness/settings.py
+++ friendlyfitness/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 import os
 from pathlib import Path
+from django.contrib.messages import constants as messages
 import dj_database_url
 
 
@@ -68,6 +69,14 @@ MIDDLEWARE = [
 ROOT_URLCONF = 'friendlyfitness.urls'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+MESSAGE_TAGS = {
+    messages.DEBUG: "alert-secondary",
+    messages.INFO: "alert-info",
+    messages.SUCCESS: "alert-success",
+    messages.WARNING: "alert-warning",
+    messages.ERROR: "alert-danger",
+}
 
 TEMPLATES = [
     {
