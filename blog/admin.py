857af7ec1907bb01187dd3ad0f69958ca29c1c6e
StephenKennyGains <stephenkennygains@gmail.com>
2022-09-24 13:23:14 +0000

    Add and install summernote for better post editing

--- blog/admin.py
+++ blog/admin.py
@@ -3,13 +3,15 @@ an admin panel and enables approval of user commenting on posts"""
 
 from django.contrib import admin
 from .models import Post
+from django_summernote.admin import SummernoteModelAdmin
 
 
 @admin.register(Post)
-class PostAdmin(admin.ModelAdmin):
+class PostAdmin(SummernoteModelAdmin):
     """ Sets the display and functionality for blog posting """
 
     prepopulated_fields = {'slug': ('title',)}
     list_filter = ('status', 'created_on')
     list_display = ('title', 'slug', 'status', 'created_on')
     search_fields = ('title', 'content')
+    summernote_fields = ('content',)
--- friendlyfitness/settings.py
+++ friendlyfitness/settings.py
@@ -55,6 +55,7 @@ INSTALLED_APPS = [
     'review',
     'blog',
     'crispy_forms',
+    'django_summernote',
     'storages',
 ]
 
