fd825f2311ca06088f957f06d07af053a4075947
StephenKennyGains <stephenkennygains@gmail.com>
2022-10-31 15:28:15 +0000

    Updated line too long in forms.py

--- services/forms.py
+++ services/forms.py
@@ -11,8 +11,7 @@ class ServiceForm(forms.ModelForm):
         model = Service
         fields = '__all__'
 
-    image = forms.ImageField(label='Image', required=False,
-                             widget=CustomClearableFileInput)
+    image = forms.ImageField(label='Image', required=False, widget=CustomClearableFileInput)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
